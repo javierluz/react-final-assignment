2723eb6f829e444b254001e7fe637da23cde945a
Javier Luz <javierluz@fixe.cl>
2021-06-25 20:51:03 -0400

    Se implementó un login simple, con Google.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,16 @@ import {
 } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store/index';
+import firebase from "firebase/app";
+import "firebase/auth";
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd
+} from "@react-firebase/auth";
+import { FirebaseDatabaseProvider } from "@react-firebase/database";
+import { config } from "./config";
 
 import Home from './containers/home/Home';
 import NoMatch from './containers/no-match/NoMatch';
@@ -21,7 +31,47 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/">
-              <Home />
+              <FirebaseAuthProvider firebase={firebase} {...config}>
+                <div>
+                  <button
+                    onClick={() => {
+                      const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+                      firebase.auth().signInWithPopup(googleAuthProvider);
+                    }}
+                  >
+                    Sign In with Google
+                  </button>
+                  <button
+                    onClick={() => {
+                      firebase.auth().signOut();
+                    }}
+                  >
+                    Sign Out
+                  </button>
+                  <FirebaseAuthConsumer>
+                    {({ isSignedIn, user, providerId }) => {
+                      return (
+                        <pre style={{ height: 300, overflow: "auto" }}>
+                          {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+                        </pre>
+                      );
+                    }}
+                  </FirebaseAuthConsumer>
+
+                  <div>
+                    <IfFirebaseAuthed>
+                      {() => {
+                        return (
+                          <FirebaseDatabaseProvider firebase={firebase} {...config}>
+                            <Home />
+                          </FirebaseDatabaseProvider>
+                        );
+                      }}
+                    </IfFirebaseAuthed>
+                  </div>
+
+                </div>
+              </FirebaseAuthProvider>
             </Route>
             <Route path="*">
               <NoMatch />
---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    apiKey: "AIzaSyA8cocgFpMjTWcIQDHU2RQwV4UulnFrd4k",
+    authDomain: "javierluz-react-final.firebaseapp.com",
+    databaseURL: "https://javierluz-react-final.firebaseio.com",
+    projectId: "javierluz-react-final",
+    storageBucket: "javierluz-react-final.appspot.com",
+    messagingSenderId: "382149300914",
+    appId: "1:382149300914:web:5b5e152d4c3b450761efab"
+};
